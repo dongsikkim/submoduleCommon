245097216c697e1116da0d0fe14ae6f7e9d7de91
dongsik kim <simsooltong@gmail.com>
2020-06-02 17:00:08 +0900

    add test sample

--- app/src/main/java/corp/ncsoft/ncapps/submoduletest/MainActivity.kt
+++ app/src/main/java/corp/ncsoft/ncapps/submoduletest/MainActivity.kt
@@ -1,11 +1,36 @@
 package corp.ncsoft.ncapps.submoduletest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.airbnb.lottie.LottieAnimationView
+import corp.ncsoft.ncapps.submoduletest.ui.loadDefaultLottie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<AppCompatButton>(R.id.button_start).setOnClickListener {
+            findViewById<LottieAnimationView>(R.id.lottie_view).apply {
+                addAnimatorListener(object : LottieAnimListener() {
+                    override fun onAnimationEnd(animation: Animator?) =
+                        super.onAnimationEnd(animation)
+                            .also { visibility = View.GONE; cancelAnimation() }
+                })
+                loadDefaultLottie()
+            }
+        }
     }
+}
+
+open class LottieAnimListener : Animator.AnimatorListener {
+    override fun onAnimationRepeat(animation: Animator?) {}
+
+    override fun onAnimationEnd(animation: Animator?) {}
+
+    override fun onAnimationCancel(animation: Animator?) {}
+
+    override fun onAnimationStart(animation: Animator?) {}
 }
