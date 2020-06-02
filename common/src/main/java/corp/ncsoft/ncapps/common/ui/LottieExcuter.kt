6f39c37e65072d71407cdd92725e35dbe1511518
dongsik kim <simsooltong@gmail.com>
2020-06-02 17:16:43 +0900

    add module

--- common/src/main/java/corp/ncsoft/ncapps/common/ui/LottieExcuter.kt
+++ common/src/main/java/corp/ncsoft/ncapps/common/ui/LottieExcuter.kt
@@ -1,10 +1,11 @@
-package corp.ncsoft.ncapps.submoduletest.ui
+package corp.ncsoft.ncapps.common.ui
 
+import android.animation.Animator
 import android.annotation.SuppressLint
 import android.view.View
 import androidx.annotation.IdRes
 import com.airbnb.lottie.LottieAnimationView
-import corp.ncsoft.ncapps.submoduletest.R
+import corp.ncsoft.ncapps.common.R
 
 @SuppressLint("ResourceType")
 fun LottieAnimationView.loadDefaultLottie(anim: LottieAnim = LottieAnim.LIKE) {
@@ -22,4 +23,14 @@ enum class LottieAnim(@IdRes val resId: Int) {
     LIKE(R.raw.lottie_like),
     FAVORITE(R.raw.lottie_favorite),
     UNLIKE(R.raw.lottie_unlike)
+}
+
+open class LottieAnimListener : Animator.AnimatorListener {
+    override fun onAnimationRepeat(animation: Animator?) {}
+
+    override fun onAnimationEnd(animation: Animator?) {}
+
+    override fun onAnimationCancel(animation: Animator?) {}
+
+    override fun onAnimationStart(animation: Animator?) {}
 }
